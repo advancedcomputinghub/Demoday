6961e65fc2fab345eb133243fd7389827a46583a
Adv. Computing Hub UWindsor <advancedcomputinghub@outlook.com>
2026-03-19 09:15:57 -0400

    Update vite.config.ts
    Updated Base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/CS-Demo-Day-Landing-2026/', // required for GitHub Pages project site
+  bbase: '/Demoday/', // required for GitHub Pages project site
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
